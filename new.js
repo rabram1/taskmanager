8ee11e384a768075880e54719bef583a80531578
rabram1 <romanabram@web.de>
2021-04-27 14:55:13 +0200

    add new task javascript

--- new.js
+++ new.js
@@ -1,1 +1,12 @@
-alert("This script is loaded");
+const formElement = document.querySelector(".form");
+const taskNameInput = document.querySelector(".form__input");
+
+function getTask() {
+  const radioGroupItem = document.querySelector(".radio-group__input:checked");
+  console.log(taskNameInput.value, radioGroupItem.value);
+}
+
+formElement.onsubmit = function (event) {
+  event.preventDefault();
+  getTask();
+};
